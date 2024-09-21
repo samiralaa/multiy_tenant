17dbca31198491a7760177ce0e9301669ea478f8
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-09-22 01:07:36 +0300

    Single Responsibility Principle

--- routes/auth.php
+++ routes/auth.php
@@ -19,3 +19,42 @@ Route::middleware('guest')->group(function () {
     Route::post('register', [RegisteredUserController::class, 'store']);
 
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
+
+    Route::get('password/reset', [PasswordResetLinkController::class, 'create'])
+                ->name('password.request');
+
+    Route::post('password/email', [PasswordResetLinkController::class, 'store'])
+                ->name('password.email');
+Route::get('password/reset/{token}', [NewPasswordController::class, 'create'])
+                ->name('password.reset');
+
+    Route::post('password/reset', [NewPasswordController::class, 'store'])
+                ->name('password.update');
+
+    Route::get('email/verify', [EmailVerificationPromptController::class, '__invoke'])
+                ->name('verification.notice');
+
+    Route::get('email/verify/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware(['auth', 'throttle:6,1'])
+                ->name('verification.send');
+
+    Route::get('password/confirm', [ConfirmablePasswordController::class, 'show'])
+                ->name('password.confirm');
+Route::post('password/confirm', [ConfirmablePasswordController::class, 'confirm'])
+                ->name('password.confirm');
+
+    Route::get('password/confirm/{token}', [ConfirmablePasswordController::class, 'confirmWithToken'])
+                ->name('password.confirm.token');
+
+    Route::get('profile', function () {
+        //
+    })->name('profile.show');
+});
